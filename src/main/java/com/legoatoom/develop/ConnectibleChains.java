a89695566326d1e7137a060b28cc1c8955179614
legoatoom <16699025+legoatoom@users.noreply.github.com>
2020-04-28 17:30:02 +0200

    Initial Version

---
+++ src/main/java/com/legoatoom/develop/ConnectibleChains.java
@@ -0,0 +1,13 @@
+package com.legoatoom.develop;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class ConnectibleChains implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
