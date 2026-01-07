d3768d3fb125f91e1bece7ff5e4b4fdb41351744
legoatoom <16699025+legoatoom@users.noreply.github.com>
2026-01-07 11:45:05 +0100

    Fix bug making catenaries invisible in walls.
    Resolves #109

--- src/main/java/com/github/legoatoom/connectiblechains/entity/ChainKnotEntity.java
+++ src/main/java/com/github/legoatoom/connectiblechains/entity/ChainKnotEntity.java
@@ -35,7 +35,6 @@ import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
@@ -269,14 +268,6 @@ public class ChainKnotEntity extends AbstractDecorationEntity implements Chainab
         double width = getType().getWidth() / 2.0;
         double height = getType().getHeight();
         setBoundingBox(new Box(getX() - width, getY(), getZ() - width, getX() + width, getY() + height, getZ() + width));
-    }
-
-    @Override
-    public boolean shouldRender(double distance) {
-        if (getWorld().getBlockState(getDecorationBlockPos()).isIn(BlockTags.WALLS)) {
-            return false;
-        }
-        return distance < 1024.0; //TODO: Determine if this needs to be changed, it used to be just true.
     }
 
     @Override
